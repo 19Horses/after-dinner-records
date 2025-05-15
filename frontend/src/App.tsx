6f64a6578d863d7cba4e1eceed5da8f96cffb447
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2025-05-15 16:51:34 +0100

    Add strapi

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,12 @@
 import { styled } from 'styled-components';
 import { Landing } from './pages/Landing';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import Home from './pages/Home';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:1337/graphql/',
+  cache: new InMemoryCache(),
+});
 
 const Wrapper = styled.div`
   width: 100vw;
@@ -8,9 +15,12 @@ const Wrapper = styled.div`
 
 const App = () => {
   return (
-    <Wrapper>
-      <Landing />
-    </Wrapper>
+    <ApolloProvider client={client}>
+      <Wrapper>
+        <Landing />
+        <Home />
+      </Wrapper>
+    </ApolloProvider>
   );
 };
 
