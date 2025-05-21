14d4540f53257ef227a5ef42e01110ff42d986d7
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2025-05-21 17:23:59 +0100

    Smooth move over to next party position

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,29 +1,39 @@
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, Sphere } from '@react-three/drei';
 import { Canvas, useFrame, useLoader } from '@react-three/fiber';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import { Vector3 } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
+const cameraPositions = {
+  initial: {
+    lookAt: new Vector3(0, 0, 0),
+    position: new Vector3(15, 1, 2),
+  },
+  nextParty: {
+    lookAt: new Vector3(-3.6, 0, 8.5),
+    position: new Vector3(-2, 0, 6.5),
+  },
+};
+
 export const Landing = () => {
   const gltf = useLoader(GLTFLoader, './backyard.glb');
-  const [isZoomedOut, setIsZoomedOut] = useState(true);
   const [doneTransitioning, setDoneTransitioning] = useState(false);
+  const [cameraPosition, setCameraPosition] = useState(cameraPositions.initial);
+  const lookAtRef = useRef(cameraPositions.initial.lookAt);
 
   const CameraController = () => {
     useFrame(({ camera }, delta) => {
-      const targetLookAt = isZoomedOut
-        ? new Vector3(0, 0, 0)
-        : new Vector3(0, 1, 0);
-      camera.lookAt(targetLookAt);
+      const targetLookAt = cameraPosition.lookAt;
+      const targetPosition = cameraPosition.position;
+
+      lookAtRef.current.lerp(targetLookAt, delta * 3);
+      camera.lookAt(lookAtRef.current);
 
       if (doneTransitioning) {
         return;
       }
-      const targetPosition = isZoomedOut
-        ? new Vector3(10, 10, 10)
-        : new Vector3(1, 1, 2);
-
       camera.position.lerp(targetPosition, delta * 3);
+
       if (camera.position.distanceTo(targetPosition) < 0.05) {
         setDoneTransitioning(true);
       }
@@ -32,20 +42,38 @@ export const Landing = () => {
   };
 
   return (
-    <Canvas camera={{ position: [-20, -20, -20] }}>
-      <CameraController />
-      <ambientLight />
-      <directionalLight position={[10, 10, 10]} />
-      <OrbitControls
-        enableZoom={false}
-        minPolarAngle={0}
-        maxPolarAngle={Math.PI / 2}
-        minAzimuthAngle={-Math.PI / 4}
-        maxAzimuthAngle={Math.PI / 4}
-        enablePan={doneTransitioning}
-        enableRotate={doneTransitioning}
-      />
-      <primitive position={[0, 0, 0]} object={gltf.scene} />
-    </Canvas>
+    <>
+      <Canvas camera={{ position: [-20, -20, -20] }}>
+        <CameraController />
+        <ambientLight />
+        <directionalLight position={[10, 10, 10]} />
+        <OrbitControls
+          enableZoom={false}
+          // minPolarAngle={0}
+          // maxPolarAngle={Math.PI / 2}
+          // minAzimuthAngle={-Math.PI / 4}
+          // maxAzimuthAngle={Math.PI / 4}
+          enablePan={doneTransitioning}
+          enableRotate={doneTransitioning}
+        />
+        <primitive position={[0, 0, 0]} object={gltf.scene} />
+        <Sphere args={[1, 16, 16]} position={[0, 0, 0]} />
+        <Sphere args={[1, 16, 16]} position={[-2, 0, 7]} />
+      </Canvas>
+      <button
+        style={{
+          position: 'fixed',
+          bottom: '24px',
+          left: '50%',
+          transform: 'translate(-50%)',
+        }}
+        onClick={() => {
+          setDoneTransitioning(false);
+          setCameraPosition(cameraPositions.nextParty);
+        }}
+      >
+        Next party
+      </button>
+    </>
   );
 };
