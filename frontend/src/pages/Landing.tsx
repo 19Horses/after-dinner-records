2eac7ea615ddbef71de79c9f1b76234743d1ada7
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2025-05-28 16:46:44 +0100

    faster spin

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -113,7 +113,7 @@ export const Landing = ({ isAtSplash }: { isAtSplash: boolean }) => {
           enableZoom={false}
           enablePan={page.id !== 'partyHistory'}
           enableRotate={page.id !== 'partyHistory'}
-          autoRotateSpeed={0.6}
+          autoRotateSpeed={0.9}
         />
         <primitive position={[0, 0, 0]} object={gltf.scene} />
         <NextPartyPoster moveTo={moveToPage} currentPage={page} />
