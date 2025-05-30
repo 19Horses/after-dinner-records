e272c69414df1b223b6f5d7d72efa8f25df6b04c
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2025-05-30 12:35:18 +0100

    Fix image int

--- frontend/src/strapiIntegration.ts
+++ frontend/src/strapiIntegration.ts
@@ -2,5 +2,7 @@ export const STRAPI_URL = import.meta.env.DEV
   ? 'http://localhost:1337'
   : 'https://efficient-diamond-bfe21915f7.strapiapp.com';
 
-export const getImageDownloadUrl = (extension: string) =>
-  `${STRAPI_URL}${extension}`;
+export const getImageDownloadUrl = (extension: string) => {
+  if (import.meta.env.DEV) return `${STRAPI_URL}${extension}`;
+  return extension;
+};
