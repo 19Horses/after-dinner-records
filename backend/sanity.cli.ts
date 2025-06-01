b8a54fe625f6c0462db530ba9532dc0eee295960
Campbell Docherty <campbellsofitsidocherty@gmail.com>
2025-06-01 15:20:39 +0100

    Auth token for cli

--- backend/sanity.cli.ts
+++ backend/sanity.cli.ts
@@ -3,11 +3,12 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: '4osu5sin',
-    dataset: 'production'
+    dataset: 'production',
   },
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
+  studioHost: 'adr',
   autoUpdates: true,
 })
